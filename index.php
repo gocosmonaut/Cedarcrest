c7eca8872c8dc3ea8d77521e84be2f9b330ec987
Daniel Deitschel <daniel@gocosmonaut.com>
2015-01-26 07:59:19 -0800

    Random CSS
    526

--- index.php
+++ index.php
@@ -4,221 +4,40 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Untitled Document</title>
 <!-- Latest compiled and minified CSS -->
+<!-- <link href="css/<?php echo mt_rand(1, 4); ?>.css" rel="stylesheet" type="text/css"/> -->
+<link rel="stylesheet" href="css/1.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-<style>
-body {
-background-attachment: fixed;
-    background-image: url(img/fremont.jpg);
-    background-position: left top;
-    background-repeat: no-repeat;
-    background-size: cover;
-	}
-</style>
 <!-- Latest compiled and minified JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-<table width="600" border="0" cellspacing="0" cellpadding="0">
-  <tr>
-    <td>     Now is the winter of our discontent
-    Made glorious summer by this sun of York;
-    And all the clouds that lour'd upon our house
-    In the deep bosom of the ocean buried.
-    Now are our brows bound with victorious wreaths;
-    Our bruised arms hung up for monuments;
-    Our stern alarums changed to merry meetings,
-    Our dreadful marches to delightful measures.
-    Grim-visaged war hath smooth'd his wrinkled front;
-    And now, instead of mounting barded steeds
-    To fright the souls of fearful adversaries,
-    He capers nimbly in a lady's chamber
-    To the lascivious pleasing of a lute.
-    But I, that am not shaped for sportive tricks,
-    Nor made to court an amorous looking-glass;
-    I, that am rudely stamp'd, and want love's majesty
-    To strut before a wanton ambling nymph;
-    I, that am curtail'd of this fair proportion,
-    Cheated of feature by dissembling nature,
-    Deformed, unfinish'd, sent before my time
-    Into this breathing world, scarce half made up,
-    And that so lamely and unfashionable
-    That dogs bark at me as I halt by them;
-    Why, I, in this weak piping time of peace,
-    Have no delight to pass away the time,
-    Unless to spy my shadow in the sun
-    And descant on mine own deformity:
-    And therefore, since I cannot prove a lover,
-    To entertain these fair well-spoken days,
-    I am determined to prove a villain
-    And hate the idle pleasures of these days.
-    Plots have I laid, inductions dangerous,
-    By drunken prophecies, libels and dreams,
-    To set my brother Clarence and the king
-    In deadly hate the one against the other:
-    And if King Edward be as true and just
-    As I am subtle, false and treacherous,
-    This day should Clarence closely be mew'd up,
-    About a prophecy, which says that 'G'
-    Of Edward's heirs the murderer shall be.
-    Dive, thoughts, down to my soul: here
-    Clarence comes.
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Launch demo modal
+</button>
 
-    Enter CLARENCE, guarded, and BRAKENBURY
-    Brother, good day; what means this armed guard
-    That waits upon your grace?
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
-CLARENCE
-
-    His majesty
-    Tendering my person's safety, hath appointed
-    This conduct to convey me to the Tower.
-
-GLOUCESTER
-
-    Upon what cause?
-
-CLARENCE
-
-    Because my name is George.
-
-GLOUCESTER
-
-    Alack, my lord, that fault is none of yours;
-    He should, for that, commit your godfathers:
-    O, belike his majesty hath some intent
-    That you shall be new-christen'd in the Tower.
-    But what's the matter, Clarence? may I know?
-
-CLARENCE
-
-    Yea, Richard, when I know; for I protest
-    As yet I do not: but, as I can learn,
-    He hearkens after prophecies and dreams;
-    And from the cross-row plucks the letter G.
-    And says a wizard told him that by G
-    His issue disinherited should be;
-    And, for my name of George begins with G,
-    It follows in his thought that I am he.
-    These, as I learn, and such like toys as these
-    Have moved his highness to commit me now.
-
-GLOUCESTER
-
-    Why, this it is, when men are ruled by women:
-    'Tis not the king that sends you to the Tower:
-    My Lady Grey his wife, Clarence, 'tis she
-    That tempers him to this extremity.
-    Was it not she and that good man of worship,
-    Anthony Woodville, her brother there,
-    That made him send Lord Hastings to the Tower,
-    From whence this present day he is deliver'd?
-    We are not safe, Clarence; we are not safe.
-
-CLARENCE
-
-    By heaven, I think there's no man is secure
-    But the queen's kindred and night-walking heralds
-    That trudge betwixt the king and Mistress Shore.
-    Heard ye not what an humble suppliant
-    Lord hastings was to her for his delivery?
-
-GLOUCESTER
-
-    Humbly complaining to her deity
-    Got my lord chamberlain his liberty.
-    I'll tell you what; I think it is our way,
-    If we will keep in favour with the king,
-    To be her men and wear her livery:
-    The jealous o'erworn widow and herself,
-    Since that our brother dubb'd them gentlewomen.
-    Are mighty gossips in this monarchy.
-
-BRAKENBURY
-
-    I beseech your graces both to pardon me;
-    His majesty hath straitly given in charge
-    That no man shall have private conference,
-    Of what degree soever, with his brother.
-
-GLOUCESTER
-
-    Even so; an't please your worship, Brakenbury,
-    You may partake of any thing we say:
-    We speak no treason, man: we say the king
-    Is wise and virtuous, and his noble queen
-    Well struck in years, fair, and not jealous;
-    We say that Shore's wife hath a pretty foot,
-    A cherry lip, a bonny eye, a passing pleasing tongue;
-    And that the queen's kindred are made gentle-folks:
-    How say you sir? Can you deny all this?
-
-BRAKENBURY
-
-    With this, my lord, myself have nought to do.
-
-GLOUCESTER
-
-    Naught to do with mistress Shore! I tell thee, fellow,
-    He that doth naught with her, excepting one,
-    Were best he do it secretly, alone.
-
-BRAKENBURY
-
-    What one, my lord?
-
-GLOUCESTER
-
-    Her husband, knave: wouldst thou betray me?
-
-BRAKENBURY
-
-    I beseech your grace to pardon me, and withal
-    Forbear your conference with the noble duke.
-
-CLARENCE
-
-    We know thy charge, Brakenbury, and will obey.
-
-GLOUCESTER
-
-    We are the queen's abjects, and must obey.
-    Brother, farewell: I will unto the king;
-    And whatsoever you will employ me in,
-    Were it to call King Edward's widow sister,
-    I will perform it to enfranchise you.
-    Meantime, this deep disgrace in brotherhood
-    Touches me deeper than you can imagine.
-
-CLARENCE
-
-    I know it pleaseth neither of us well.
-
-GLOUCESTER
-
-    Well, your imprisonment shall not be long;
-    Meantime, have patience.
-
-CLARENCE
-
-    I must perforce. Farewell.
-
-    Exeunt CLARENCE, BRAKENBURY, and Guard
-
-GLOUCESTER
-
-    Go, tread the path that thou shalt ne'er return.
-    Simple, plain Clarence! I do love thee so,
-    That I will shortly send thy soul to heaven,
-    If heaven will take the present at our hands.
-    But who comes here? the new-deliver'd Hastings?
-
-    Enter HASTINGS
-</td>
-  </tr>
-</table>
 </body>
 </html>
